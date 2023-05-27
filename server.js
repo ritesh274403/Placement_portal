5433ca5bc65de89a9bdf2045c70c983d50109a47
ritesh <ritesh274403@gmail.com>
2023-05-27 11:49:34 +0530

    initial

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose
 const sessionPass = process.env.SESSION_PASS;
 app.use(
 	session({
-		secret: sessionPass,
+		secret: 'sessionPassSuperSecretPassowardForHiringPortal',
 		resave: false,
 		saveUninitialized: false,
 		// store: MongoStore.create({
@@ -86,6 +86,6 @@ app.get('*', (req, res) => {
 
 //PORT CONNECTION
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-	console.log('server started');
+app.listen(3000, () => {
+	console.log('Server is running on port 3000');
 });
